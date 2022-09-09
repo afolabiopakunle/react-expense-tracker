96e99eda47dbbdf0c6d2c1387f2d83988444649c
Afolabi <opakunleafolabi@gmail.com>
2022-09-09 04:02:01 +0100

    items added

---
+++ src/components/Expenses/ExpensesList/ExpensesList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ExpenseItem from "../ExpenseItem";
+import './ExpensesList.css';
+
+function ExpensesList(props) {
+    return (
+        <div>
+            <ExpenseItem />
+        </div>
+    );
+}
+
+export default ExpensesList;
